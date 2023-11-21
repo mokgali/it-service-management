f422b032cb8380a0fb0bfce820ae0394ccde8176
mokgali <mokgali@gmail.com>
2023-11-21 10:03:01 +0200

    Added a JPA Users Repository and its corresponding Model

---
+++ src/main/java/com/itservicemanagement/spring/upskillspringframework/repository/User.java
@@ -0,0 +1,52 @@
+package com.itservicemanagement.spring.upskillspringframework.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+
+    protected User(){
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+      }
+    
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    
+}
