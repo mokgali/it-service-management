2ac9f6c825e8a777963d1d330c929bc16bf90b9e
mokgali <mokgali@gmail.com>
2023-11-22 15:16:33 +0200

    Code cleanup

--- frontend/src/components/appHeader.js
+++ frontend/src/components/appHeader.js
@@ -2,12 +2,10 @@ import Menu  from "./menu"
 import Styles from "./appHeader.module.css"
 
 function AppHeader({logo}){
-console.log(Styles.appHeader)
-console.log(Styles.appLogo)
 return(
    <header className= {Styles.appHeader}>
     <img src={logo} className={Styles.appLogo} alt="logo" />   
-    <span>IT Service Management</span>
+    <span>IT Service Management</span>       
     <Menu></Menu>
   </header> 
 )
