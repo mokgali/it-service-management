ca8a3e649aaff792fc886fb9b37ace5b922a206c
mokgali <mokgali@gmail.com>
2023-11-22 15:17:25 +0200

    Added handlers for login and register pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,16 +3,45 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Button } from 'react-bootstrap';
 import Styles from './Login.module.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+
+  const [email,setEmail]= useState("");
+  const [password,setPassword]= useState("");
+  const navigate=useNavigate();
+
+
+  function handleEmailChange(e){
+    e.preventDefault();
+   setEmail(e.target.value)  
+   
+  }
+  function handlePasswordChange(e){
+    e.preventDefault();  
+    setPassword(e.target.value)  
+
+  }
+
+  async function handleLogin(e){
+    e.preventDefault();   
+    const url=`/clients/${email}`;
+    const response = await fetch(url,{method:'GET'});  
+    if(response.status){
+     navigate('/loggedTickets')
+    }
+
+
+  }
   return (
     <Form className={Styles.Login}>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+       <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
         <Form.Label column sm="2">
           Email
         </Form.Label>
         <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue="email@example.com" />
+          <Form.Control   onChange={e=>handleEmailChange(e)}/>
         </Col>
       </Form.Group>
 
@@ -21,11 +50,11 @@ function Login() {
           Password
         </Form.Label>
         <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" onChange={e=>handlePasswordChange(e)}/>
         </Col>
       </Form.Group>
       <Form.Group  className="mb-3" >
-        <Button  className="float-end" size="lg">Submit</Button>    
+        <Button  className="float-end" size="lg" onClick={(e=>handleLogin(e))}>Submit</Button>    
      </Form.Group>   
     </Form>       
   );
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,16 +3,72 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Button } from 'react-bootstrap';
 import Styles from './Login.module.css'
+import { useState } from 'react';
 
-function Login() {
+
+
+function Register() {
+
+  const [email,setEmail]= useState("");
+  const [password,setPassword]= useState("");
+  const [name,setName]= useState("");
+  const [verifyPassword,setVerifyPassword]= useState("");
+
+
+  async function handleRegister(e){
+    e.preventDefault();
+    console.log(email)
+  
+   
+      const response = await fetch("/clients",{
+        method:'POST',
+        headers:{
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body:JSON.stringify({ name:name,email: email})
+      });           
+
+  }
+  
+  function handleNameChange(e){
+    e.preventDefault();
+   setName(e.target.value)  
+  } 
+
+   function handleEmailChange(e){
+    e.preventDefault();
+   setEmail(e.target.value)  
+   
+  }
+  function handlePasswordChange(e){
+    e.preventDefault();  
+    setPassword(e.target.value)  
+
+  }
+  function handleVerifyPasswordChange(e){
+    e.preventDefault();  
+    setPassword(e.target.value)  
+
+  }
+
+
   return (
-    <Form className={Styles.Login}>
+    <Form className={Styles.Login} onSubmit={(e=>handleRegister(e))}>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+        <Form.Label column sm="2">
+          Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control   onChange={e=>handleNameChange(e)}/>
+        </Col>
+      </Form.Group>
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
         <Form.Label column sm="2">
           Email
         </Form.Label>
         <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue="email@example.com" />
+          <Form.Control   onChange={e=>handleEmailChange(e)}/>
         </Col>
       </Form.Group>
 
@@ -21,22 +77,22 @@ function Login() {
           Password
         </Form.Label>
         <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" onChange={e=>handlePasswordChange(e)}/>
         </Col>
       </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+    {   <Form.Group as={Row} className="mb-3" controlId="formPlaintextVerifyPassword">
         <Form.Label column sm="5">
           Verify Password
         </Form.Label>
         <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" onChange={e=>handleVerifyPasswordChange(e)}/>
         </Col>
-      </Form.Group>
+      </Form.Group> }
       <Form.Group  className="mb-3" >
-        <Button  className="float-end" size="lg">Submit</Button>    
+        <Button  className="float-end" size="lg" type="submit" onClick={(e=>handleRegister(e))}>Submit</Button>    
      </Form.Group>   
     </Form>       
   );
 }
 
-export default Login;
+export default Register;
