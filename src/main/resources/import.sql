2f4f0da0aa0d04f455489447688c48c319210397
mokgali <mokgali@gmail.com>
2023-11-23 11:31:45 +0200

    Configured the data source and added sql scripts to prepopulate the database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO Client (email,name) VALUES ('khalele@gmail.com','Khalele');
+INSERT INTO Client (email,name) VALUES ('ruthie@gmail.com','Ruth');
+
+
+INSERT INTO TICKET (ASSIGNED_TO,CATEGORY,CLOSING_DATE,CREATED_BY,OPENING_DATE,STATUS) VALUES ('John','software','','khalele@gmail.com','2023-11-16','pending')
+INSERT INTO TICKET (ASSIGNED_TO,CATEGORY,CLOSING_DATE,CREATED_BY,OPENING_DATE,STATUS) VALUES ('Peter','hardware','2023-09-12','khalele@gmail.com','2023-10-01','resolved')
+INSERT INTO TICKET (ASSIGNED_TO,CATEGORY,CLOSING_DATE,CREATED_BY,OPENING_DATE,STATUS) VALUES ('Emmanuel','software','','khalele@gmail.com','2023-09-01','pending')
