ceea7c2316667dbf18ecc842141711d6ddeb72ce
mokgali <mokgali@gmail.com>
2023-11-30 14:09:06 +0200

    updated effect to do api calls whenever the filter button is clicked for a new option

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -10,43 +10,59 @@ import Styles from './dashboard.module.css'
 
 function Dashboard(){
 
-    const [data,setData]=useState('');
-    const userName='khalele@gmail.com';
-    const [queryDate,setQueryDate]= useState(new Date());
-    const todayDate= new Date();
-    todayDate.setDate((todayDate.getDate()-7))
-    const [formattedDate] = todayDate.toISOString().split('T');
-    const selectedFilter= useState('This Week');    
+    const [data,setData]=useState(null);
+    const userName='khalele@gmail.com';    
+    const [selectedReportRange,setSelectedReportRange]= useState('This Week');    
     const [statsTotals,setStatsTotals]=useState([{name:"Open",total:0},{name:"Pending",total:0},{name:"Closed",total:0}])
-     
+      
      useEffect(()=>{
+  
+        function generateGenericTotalsFromData(){
+            const dataByStatus= ["Open","Pending","Closed"].map(element => {
+                var totalForStatus=0;
+                if(data!==null && data.length>0)
+                {data.forEach(dataElement=>{
+                       if(element===dataElement.status){
+                        totalForStatus++;
+                       }
+                })
+                return{
+                    name:element,
+                    total:totalForStatus
+                }
+            }
+            });  
+             setStatsTotals(dataByStatus); 
+          }
+          
         async function fetchTicketsForDateRange(){
+        const todayDate= new Date();
+        todayDate.setDate((todayDate.getDate()-7))
+        if(selectedReportRange==="Past two weeks")
+        {
+        todayDate.setDate((todayDate.getDate()-14))
+        }
+        if(selectedReportRange==="This Month")
+        {
+        todayDate.setDate((todayDate.getDate()-30))
+        }
+        if(selectedReportRange==="Last Month")
+        {
+        todayDate.setDate((todayDate.getDate()-30))
+        }        
+        const [formattedDate] = todayDate.toISOString().split('T');
+
         const url=`/tickets/${userName}/${formattedDate}`;
         const response = await fetch(url,{method:'GET'}); 
         const responseData = await response.json();          
-        setData(data=>responseData);   
-        console.log(data) 
-        if(data){    
-        generateGenericTotalsFromData() 
-        }       
-        }
-        fetchTicketsForDateRange()        
-      })
+        setData(responseData);          
+        }   
+        fetchTicketsForDateRange();   
+        generateGenericTotalsFromData();      
+      },[selectedReportRange]);      
 
-      function generateGenericTotalsFromData(){
-        const dataByStatus= ["Open","Pending","Closed"].map(element => {
-            var totalForStatus=0;
-            data.forEach(dataElement=>{
-                   if(element===dataElement.status){
-                    totalForStatus++;
-                   }
-            })
-            return{
-                name:element,
-                total:totalForStatus
-            }
-        });  
-         setStatsTotals(dataByStatus); 
+      function handleReportRangeSelection(selectedReportRange){ 
+        setSelectedReportRange(e=>selectedReportRange);
       }
 
 return(
@@ -56,23 +72,22 @@ return(
        <Col className={Styles.ReportsCategory} >
             <Row >
             <Col xs={6}> <h3>Dashboard</h3></Col>
-            <Col xs={6}><ReportRange/></Col>    
+            <Col xs={6}><ReportRange onReportRangeSelection={handleReportRangeSelection}/></Col>    
             </Row>  
             <Row>
             <Col>  <ReportFilters/></Col>
             </Row>
-            <Row className={Styles.statsBar}>
-
-             {statsTotals.map(statsTotalObject=>{
-                  return (<Col className={Styles.statDiv} >
+     {/*        <Row className={Styles.statsBar}>
+             {statsTotals!==null && statsTotals.length>0? statsTotals.map(statsTotalObject=>{
+                  return (<Col className={Styles.statDiv} key={statsTotalObject.name}>
                    <h5>{statsTotalObject.name} Tickets</h5>    
                    <h5>{statsTotalObject.total} </h5>       
                    </Col>
                   )
                   }
-                )}                       
-            </Row>
-            {data!==null && data.length>0? 
+                ):<></>}                       
+            </Row> */}
+            {(data!==null||data!==null) && data.length>0? 
             <Row>
             <Col><ReportBarChart chartData={data}/></Col>    
             <Col><ReportPieChart chartData={statsTotals}/></Col>          
