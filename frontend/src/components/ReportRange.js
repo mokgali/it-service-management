81a24d6df1e109d7a2fe5dcb3a989be681c44de3
mokgali <mokgali@gmail.com>
2023-11-30 14:05:12 +0200

    Added handler for search buttons

--- frontend/src/components/ReportRange.js
+++ frontend/src/components/ReportRange.js
@@ -1,13 +1,23 @@
 import { ButtonGroup,Button } from "react-bootstrap"
+import { useState } from "react";
 
-function ReportRange(){
+function ReportRange({onReportRangeSelection}){
+    //const [selectedQueryRange,setSelectedQueryRange]= useState('');
+
+    function handleQuerySelection(e){
+        //setSelectedQueryRange(selectedQueryRange=>e.targetElement.value)
+        //Create the cutoff date for which we want to query by
+        onReportRangeSelection(e.target.value)
+
+    }
+
    return( <ButtonGroup aria-label="Report Range">
-    <Button variant="outline-primary">This Week</Button>
-    <Button variant="outline-primary">Past two weeks</Button>
-    <Button variant="outline-primary">This Month</Button>
-    <Button variant="outline-primary">Last Month</Button>
-    <Button variant="outline-primary">This Year</Button>
-    <Button variant="outline-primary">Last Year</Button>
+    <Button variant="outline-primary" value="This Week" onClick={e=>handleQuerySelection(e)}>This Week</Button>
+    <Button variant="outline-primary" value="Past two weeks" onClick={e=>handleQuerySelection(e)}>Past two weeks</Button>
+    <Button variant="outline-primary" value="This Month" onClick={e=>handleQuerySelection(e)}>This Month</Button>
+    <Button variant="outline-primary" value="Last Month" onClick={e=>handleQuerySelection(e)}>Last Month</Button>
+    <Button variant="outline-primary" value="This Year" onClick={e=>handleQuerySelection(e)}>This Year</Button>
+    <Button variant="outline-primary" value="Last Year" onClick={e=>handleQuerySelection(e)}>Last Year</Button>
     </ButtonGroup> 
     )
 }
