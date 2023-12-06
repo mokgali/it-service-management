0897e64dfd46237b0a893a4b57aae3f56923c639
mokgali <mokgali@gmail.com>
2023-12-06 08:52:31 +0200

    Implemented Spring security UserDetails interface

--- src/main/java/com/itservicemanagement/spring/upskillspringframework/repository/Client.java
+++ src/main/java/com/itservicemanagement/spring/upskillspringframework/repository/Client.java
@@ -1,40 +1,51 @@
 package com.itservicemanagement.spring.upskillspringframework.repository;
 
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Client {      
+public class Client implements UserDetails {      
     
     @Id
-    private String email;
-    private String name;
+    private String userName;
+    private String password;
+    private String role;
 
-    protected Client(){
-
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       
+       return Set.of();
     }
-
-    public Client(String name, String email) {
-        this.name = name;
-        this.email = email;
-      }  
+    @Override
+    public String getPassword() {
+       return password;
+    }
+    @Override
+    public String getUsername() {      
+        return userName;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+       return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+     return true;
+    }
+    @Override
+    public boolean isEnabled() {
+       return true;
+    }
  
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
     
 }
