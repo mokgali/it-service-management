8c079765f6a41fec97bc3e3340f9cf2d6a1fc9cd
mokgali <mokgali@gmail.com>
2023-12-06 08:55:20 +0200

    Removed JdbcUserDetailsManager from security config because we are using our own implementation of UserDetailsService

--- src/main/java/com/itservicemanagement/spring/upskillspringframework/SecurityConfiguration.java
+++ src/main/java/com/itservicemanagement/spring/upskillspringframework/SecurityConfiguration.java
@@ -4,13 +4,15 @@ package com.itservicemanagement.spring.upskillspringframework;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -20,10 +22,18 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfiguration  {
 
-    @Autowired
-    DataSource dataSource;
+/*     @Bean
+    DataSource dataSource(){
+    return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("ticketsmanagement")
+                .setUrl
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
 
-@Bean
+    }
+ */
+    @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((authorize) -> authorize
@@ -32,16 +42,18 @@ public class SecurityConfiguration  {
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/")).permitAll()
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
 				.anyRequest().authenticated()
-			)
+                
+			)      
         .csrf(AbstractHttpConfigurer::disable)
         .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 	return http.build();
 	}
 
-@Bean
+/* @Bean
 JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource){
  JdbcUserDetailsManager jdbcUserDetailsManager =new JdbcUserDetailsManager(dataSource)  ;
+ jdbcUserDetailsManager.loadUserByUsername(null)
  return jdbcUserDetailsManager ;
-}
+} */
     
 }
